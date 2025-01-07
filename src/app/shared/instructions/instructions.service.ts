860e0e379b7471225b4ad36af3ad467397a3eee1
maxime.coustes <maxime.coustes@hotmail.fr>
2025-01-07 15:40:58 +0100

    fix instrctions issues du to logical and mechanism changes during refacto

---
+++ src/app/shared/instructions/instructions.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { VoiceService } from '../voices/voice.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstructionsService {
+
+  noInstructions: boolean = false; // Désactiver les instructions si true
+  instructionId: any; // Pour l'instruction répétée
+  // Configuration des instructions
+  instructionInterval = 3; // Intervalle par défaut (toutes les 10 secondes)
+  instructionMinValue = 1; // Valeur min du chiffre prononcé
+  instructionMaxValue = 6; // Valeur max du chiffre prononcé
+  instructionTimer: any;    // Timer pour les instructions orales
+
+  constructor(public voiceService: VoiceService) { }
+
+  // Start the instruction timer
+  public startInstructionTimer(currentPhase: string) {
+    if (this.noInstructions) {
+      clearInterval(this.instructionId); // Arrête les instructions si désactivées
+      return;
+    }
+    this.instructionTimer = setInterval(() => {
+      if (currentPhase === 'Active') {
+        const randomValue = Math.floor(
+          Math.random() * (this.instructionMaxValue - this.instructionMinValue + 1)
+        ) + this.instructionMinValue;
+        this.speakInstruction(randomValue.toString());
+      }
+    }, this.instructionInterval * 1000);
+  }
+
+  // Use SpeechSynthesis to give oral instructions
+  public speakInstruction(text: string) {
+    if (this.noInstructions) {
+      return; // Ne joue aucune instruction si "Aucune instruction" est activée
+    }
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    // Définit la voix si elle est sélectionnée
+    const voice = this.voiceService.availableVoices.find(v => v.name === this.voiceService.selectedVoice);
+    if (voice) {
+      utterance.voice = voice;
+    }
+
+    // Parle
+    window.speechSynthesis.speak(utterance);
+  }
+
+}
