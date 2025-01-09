ef38f862283d8191b973f4accaa4489514beb5aa
maxime.coustes <maxime.coustes@hotmail.fr>
2025-01-09 16:40:53 +0100

    add parameter for displaying rounds

--- src/feature/components/clock/clock.component.ts
+++ src/feature/components/clock/clock.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ClockInstance } from './clock-instance';  // Assure-toi que le chemin est correct
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'clock',
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.css']
+  styleUrls: ['./clock.component.css'],
+  imports: [ CommonModule ]
 })
 export class ClockComponent {
   @Input() clockInstance?: ClockInstance;  // Marquer comme optionnelle
@@ -12,6 +13,7 @@ export class ClockComponent {
   @Input() currentRound?: number = 0;
   @Input() timeLeft?: number = 0;
   @Input() currentPhase?: string = '';
+  @Input() displayRound?: boolean = true;
 
   constructor() {
     // Ne pas initialiser ici, car l'instance est fournie par le parent.
