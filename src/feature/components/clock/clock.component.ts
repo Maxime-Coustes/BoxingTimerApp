bd2ef08c21bff5099b705bdcc604a70a731d08b6
maxime.coustes <maxime.coustes@hotmail.fr>
2025-01-13 16:24:13 +0100

    code clean

--- src/feature/components/clock/clock.component.ts
+++ src/feature/components/clock/clock.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { ClockInstance } from './clock-instance';  // Assure-toi que le chemin est correct
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'clock',
@@ -8,13 +7,9 @@ import { CommonModule } from '@angular/common';
   imports: [ CommonModule ]
 })
 export class ClockComponent {
-  @Input() clockInstance?: ClockInstance;  // Marquer comme optionnelle
   @Input() rounds?: number = 0;
   @Input() currentRound?: number = 0;
   @Input() timeLeft?: number = 0;
   @Input() currentPhase?: string = '';
   @Input() displayRound?: boolean = true;
-
-  
-
 }
