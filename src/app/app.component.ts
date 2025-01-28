b52846f0a968ca0af1324c0a588b3635c24399ae
maxime.coustes <maxime.coustes@hotmail.fr>
2025-01-28 15:29:20 +0100

    push all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { WheelTimer } from './shared/wheel-timer/wheel-timer.component';
 import { BoxingTimerComponent } from '../feature/components/boxingTimer/boxing-timer.component';
+import { TabataComponent } from "../feature/components/tabata/tabata.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, BoxingTimerComponent, WheelTimer],
+  imports: [CommonModule, BoxingTimerComponent, WheelTimer, TabataComponent],
 
 })
 export class AppComponent {
